6d85e0d480f258b3ea3186f0502ffbad0542f704
Diogo Gomes <dgomes@ua.pt>
2021-02-21 21:45:59 +0000

    sync with PR

--- custom_components/kmtronic/switch.py
+++ custom_components/kmtronic/switch.py
@@ -1,56 +1,35 @@
 """KMtronic Switch integration."""
-from datetime import timedelta
-import logging
-
-import aiohttp
-import async_timeout
 
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-    UpdateFailed,
-)
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DATA_HOST, DATA_HUB, DOMAIN, MANUFACTURER
-
-_LOGGER = logging.getLogger(__name__)
+from .const import DATA_COORDINATOR, DATA_HOST, DATA_HUB, DOMAIN
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Config entry example."""
+    coordinator = hass.data[DOMAIN][entry.entry_id][DATA_COORDINATOR]
     hub = hass.data[DOMAIN][entry.entry_id][DATA_HUB]
     host = hass.data[DOMAIN][entry.entry_id][DATA_HOST]
     await hub.async_get_relays()
 
-    async def async_update_data():
-        try:
-            async with async_timeout.timeout(10):
-                await hub.async_update_relays()
-        except aiohttp.client_exceptions.ClientConnectorError as err:
-            raise UpdateFailed(f"Error communicating with API: {err}") from err
-
-    coordinator = DataUpdateCoordinator(
-        hass,
-        _LOGGER,
-        name=f"{MANUFACTURER} {hub.name}",
-        update_method=async_update_data,
-        update_interval=timedelta(seconds=30),
-    )
-
     async_add_entities(
-        [KMtronicSwitch(coordinator, host, relay) for relay in hub.relays]
+        [
+            KMtronicSwitch(coordinator, host, relay, entry.unique_id)
+            for relay in hub.relays
+        ]
     )
 
 
 class KMtronicSwitch(CoordinatorEntity, SwitchEntity):
     """KMtronic Switch Entity."""
 
-    def __init__(self, coordinator, host, relay):
+    def __init__(self, coordinator, host, relay, config_entry_id):
         """Pass coordinator to CoordinatorEntity."""
         super().__init__(coordinator)
         self._host = host
         self._relay = relay
+        self._config_entry_id = config_entry_id
 
     @property
     def available(self) -> bool:
@@ -65,7 +44,7 @@ class KMtronicSwitch(CoordinatorEntity, SwitchEntity):
     @property
     def unique_id(self) -> str:
         """Return the unique ID of the entity."""
-        return f"{self._host}_relay{self._relay.id}"
+        return f"{self._config_entry_id}_relay{self._relay.id}"
 
     @property
     def entity_registry_enabled_default(self) -> bool:
